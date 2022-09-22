b0e8df7dcf004972c9f613ec00491b05b34769fb
SodikulPadli <sodikulfadly91@gmail.com>
2022-09-22 14:29:04 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// dotenv
+require('dotenv').config();
+
+// express
+const express = require('express');
+
+// router
+const router = require('./src/routes');
+
+// cors
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1', router);
+
+app.get('/', (req, res) => {
+  res.send('Hello Developer!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listenin on port ${port}`);
+});
